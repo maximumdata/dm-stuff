f19b5e4dc0a6bd3d30c82d7a5ad3695e0974f66c
Mike Dettmer <mike@mikedettmer.com>
2018-05-15 15:03:05 -0400

    adds name generators by race and sex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import Controllers from './Controllers.js';
 import './App.css';
-import needle from 'needle';
 
 class App extends Component {
   render() {
     return (
-        <StatueGenerator buttonText="Get Statue"/>
+        <div className="container">
+            <StatueGenerator buttonText="Get Statue"/>
+            <Names />
+        </div>
       // <div className="App">
       //   <header className="App-header">
       //     <img src={logo} className="App-logo" alt="logo" />
@@ -15,7 +17,7 @@ class App extends Component {
       //   <p className="App-intro">
       //     To get started, edit <code>src/App.js</code> and save to reload.
       //   </p>
-      // </div>
+      // </div>2
     );
   }
 }
@@ -25,7 +27,6 @@ class App extends Component {
 class StatueGenerator extends React.Component {
     constructor(props) {
         super(props);
-        // this.props.handleClick = this.handleClick.bind(this);
         this.handleClick = this.handleClick.bind(this);
         this.state = {
             desc: '',
@@ -34,10 +35,16 @@ class StatueGenerator extends React.Component {
     }
     handleClick() {
         this.setState((prevState) => { return {desc: 'loading', trigger: ''}})
-        needle('get', 'https://mikedettmer.com/dm/statue')
-            .then((res) => {
-                this.setState((prevState) => res.body);
+        Controllers.getStatue().then((res) => {
+            this.setState((prevState) => res.body);
+        }).catch((err) => {
+            this.setState(() => {
+                return {
+                    desc: 'error',
+                    trigger: 'error'
+                }
             });
+        });
     }
     getContent() {
         if(!this.state.desc) {
@@ -52,9 +59,7 @@ class StatueGenerator extends React.Component {
                     <div>{this.state.desc}</div>
                     <h5>Trigger:</h5>
                     <small>{this.state.trigger}</small>
-                    <br />
-                    <br />
-                    <br />
+                    <br /><br />
                     <button onClick={this.handleClick}>{this.props.buttonText}</button>
                 </div>
             )
@@ -63,6 +68,83 @@ class StatueGenerator extends React.Component {
     render() {
         return this.getContent()
     }
+}
+
+class Names extends React.Component {
+    constructor(props) {
+        super(props);
+        this.races = ['Dragonborn', 'Human', 'Dwarf', 'Elf', 'Gnome', 'Halfling', 'Tiefling', 'HalfOrc'];
+    }
+    render() {
+        return (
+            <div className="names">
+                {
+                    this.races.reduce((pairs, book, index) => { // split the books into pairs
+                        if(index % 4 === 0) {
+                            pairs.push([]);
+                        }
+                        pairs[pairs.length - 1].push(book);
+                        return pairs;
+                    }, []).map((pair, index) => ( // map the pairs to row
+                        <NamesRow key={ index } names={ pair } />
+                    ))
+                }
+            </div>
+        )
+        // return (
+        //     <div className="row names">
+        //         {
+        //             this.races.map((race, index) => <NameGenerator key={index} race={race} />)
+        //         }
+        //     </div>
+        // )
+    }
+}
+
+class NamesRow extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                {
+                    this.props.names.map((name, index) =>  <NameGenerator key={index} race={name}/>)
+                }
+            </div>
+        )
+    }
+}
+class NameGenerator extends React.Component {
+     constructor(props) {
+         super(props);
+         this.onClick = this.onClick.bind(this);
+         this.state = {
+             firstName: '',
+             lastName: ''
+         }
+     }
+     onClick(e) {
+         let sex = e.target.dataset.sex;
+         Controllers.getName(this.props.race.toLowerCase(), sex).then((res) => {
+             this.setState((prevState) => res.body);
+         }).catch((err) => {
+             console.log('err', err);
+             this.setState((prevState) => {
+                 return {
+                     firstName: 'error',
+                     lastName: 'error'
+                 }
+             });
+         });
+     }
+     render() {
+         return (
+             <div className="col-sm-12 col-md-6 col-lg-3 name">
+                 <h5>{this.props.race}</h5>
+                 <div className="name-out">{this.state.firstName} {this.state.lastName}</div>
+                 <button onClick={this.onClick} data-sex="male">Male</button>
+                 <button onClick={this.onClick} data-sex="female">Female</button>
+             </div>
+         )
+     }
 }
 
 export default App;
--- src/Controllers.js
+++ src/Controllers.js
@@ -1,20 +1,35 @@
+// const needle = require('needle');
+// const cheerio = require('cheerio');
+//
+// const getStatue = () => {
+//   return needle('get', 'http://warpwalkers.com/2017/statuegenerator/')
+//   .then((res) => {
+//     const $ = cheerio.load(res.body);
+//     let desc = $('h3 .su-box-content').text().trim();
+//     let trigger = $('#content > .su-box.su-box-style-default > .su-box-content.su-clearfix').first().text().trim();
+//     return { desc, trigger };
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// }
+//
+//
+// export default {
+//     getStatue
+// };
+
 const needle = require('needle');
-const cheerio = require('cheerio');
 
 const getStatue = () => {
-  return needle('get', 'http://warpwalkers.com/2017/statuegenerator/')
-  .then((res) => {
-    const $ = cheerio.load(res.body);
-    let desc = $('h3 .su-box-content').text().trim();
-    let trigger = $('#content > .su-box.su-box-style-default > .su-box-content.su-clearfix').first().text().trim();
-    return { desc, trigger };
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    return needle('get', 'https://mikedettmer.com/dm/statue');
 }
 
+const getName = (race, sex) => {
+    return needle('get', `https://mikedettmer.com/dm/name?race=${race}&sex=${sex}`)
+}
 
 export default {
-    getStatue
-};
+    getStatue,
+    getName
+}
